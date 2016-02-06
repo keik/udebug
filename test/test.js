0f3e398925319eeadad79258cd6f0e0db967da18
keik <k4t0.kei@gmail.com>
2016-02-07 02:44:34 +0900

    Add tests

--- test/test.js
+++ test/test.js
@@ -1,14 +1,22 @@
 var test = require('tape'),
     read = require('fs').readFileSync,
     path = require('path'),
-    fixture = function(f) { return read(path.join(__dirname, f), 'utf8') }
+    fixture = function(f) {
+      return read(path.join(__dirname, 'fixtures', f), 'utf8')
+    }
 
 var undebug = require('../')
+
 test('undebug', function(t) {
 
   t.test('should remove requiring `debug`', function(t) {
-    t.equal(undebug(fixture('./fixtures/requiring.js')),
-            fixture('./fixtures/requiring-expected.js').trim())
+
+    t.equal(undebug(fixture('./requiring.js')),
+            fixture('./requiring-expected.js').trim())
+
+    t.equal(undebug(fixture('./call.js')),
+            fixture('./call-expected.js').trim())
+
     t.end()
   })
 })
