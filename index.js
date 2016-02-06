ed5281d76017787d196f783f356106d7ecf123ed
keik <k4t0.kei@gmail.com>
2016-02-07 03:27:41 +0900

    Detect debug assigned Identifiers

--- index.js
+++ index.js
@@ -11,37 +11,63 @@ var declarationCode = 'require("debug")',
 
 module.exports = function undebug(code) {
   var ast = esprima.parse(code, {sourceType: 'module'})
-  var removes = []
-
+  var removee
+  var assignedIds = []
+debugger
   estraverse.replace(ast, {
 
     enter: function(node, parent) {
     },
 
     leave: function(node, parent) {
+      console.log(node.type, node.value || '*', node.name || '*');
+      console.log(removee, assignedIds)
       if (deepEqual(espurify(node), a_requireDebugExp)) {
-        removes.push(parent)
+        console.log('0-0')
+        removee = parent
+        console.log('aaa', parent.type);
+        if (parent.type === syntax.VariableDeclarator)
+          assignedIds.push(parent.id.name)
         this.skip()
         return
       }
 
-      if (removes.indexOf(node) > -1) {
+      if (node === removee) {
         if (node.type === syntax.CallExpression) {
-          removes.push(parent)
+          console.log('0-1')
+          removee = parent
+          if (parent.type === syntax.VariableDeclarator)
+            assignedIds.push(parent.id.name)
           this.skip()
         } else {
           this.remove()
+          removee = null
         }
         return
       }
 
       if (node.type === syntax.VariableDeclaration
-          && node.declarations.length === 0)
+          && node.declarations.length === 0) {
+        console.log(2)
         this.remove()
-      return
+        removee = null
+        return
+      }
     }
 
   })
 
   return escodegen.generate(ast)
 }
+
+
+var code = `
+var d1 = require("debug")
+require('debug')('a')
+var d2 = require('debug')('a')
+var d3 = require("debug"),
+    fs = require('fs'),
+    d4 = require("debug"),
+    d5 = require("debug")('a')
+`
+console.log(module.exports(code))
