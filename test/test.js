33a244b947cb52c1efad39c96bf6df729578e958
keik <k4t0.kei@gmail.com>
2016-02-24 19:44:47 +0900

    Rename

--- test/test.js
+++ test/test.js
@@ -9,27 +9,27 @@ var udebug = require('../')
 
 test('udebug', function(t) {
 
-  t.test('should remove requiring `debug`', function(t) {
-    t.equal(udebug(fixture('./requiring-1.js')),
-            fixture('./requiring-1-expected.js'), 'requiring debug are removed (pattern 1)')
-    t.equal(udebug(fixture('./requiring-2.js')),
-            fixture('./requiring-2-expected.js'), 'requiring debug are removed (pattern 2)')
-    t.equal(udebug(fixture('./requiring-3.js')),
-            fixture('./requiring-3-expected.js'), 'requiring debug are removed (pattern 3)')
-    t.equal(udebug(fixture('./requiring-4.js')),
-            fixture('./requiring-4-expected.js'), 'requiring debug are removed (pattern 4)')
-    t.equal(udebug(fixture('./requiring-5.js')),
-            fixture('./requiring-5-expected.js'), 'requiring debug are removed (pattern 5)')
-    t.equal(udebug(fixture('./requiring-6.js')),
-            fixture('./requiring-6-expected.js'), 'requiring debug are removed (pattern 6)')
+  t.test('should remove require `debug`', function(t) {
+    t.equal(udebug(fixture('./require-1.js')),
+            fixture('./require-1-expected.js'), 'require debug are removed (pattern 1)')
+    t.equal(udebug(fixture('./require-2.js')),
+            fixture('./require-2-expected.js'), 'require debug are removed (pattern 2)')
+    t.equal(udebug(fixture('./require-3.js')),
+            fixture('./require-3-expected.js'), 'require debug are removed (pattern 3)')
+    t.equal(udebug(fixture('./require-4.js')),
+            fixture('./require-4-expected.js'), 'require debug are removed (pattern 4)')
+    t.equal(udebug(fixture('./require-5.js')),
+            fixture('./require-5-expected.js'), 'require debug are removed (pattern 5)')
+    t.equal(udebug(fixture('./require-6.js')),
+            fixture('./require-6-expected.js'), 'require debug are removed (pattern 6)')
     t.end()
   })
 
-  t.test('should remove calling `debug`', function(t) {
-    t.equal(udebug(fixture('./call.js')),
-            fixture('./call-expected.js'), 'calling debug are removed')
-    t.equal(udebug(fixture('./scope.js')),
-            fixture('./scope-expected.js'), 'calling debug are removed with scope sensitive')
+  t.test('should remove calling `debug` imported by require', function(t) {
+    t.equal(udebug(fixture('./require-and-call.js')),
+            fixture('./require-and-call-expected.js'), 'calling debug are removed')
+    t.equal(udebug(fixture('./require-and-call-on-scope.js')),
+            fixture('./require-and-call-on-scope-expected.js'), 'calling debug are removed with scope sensitive')
     t.end()
   })
 
