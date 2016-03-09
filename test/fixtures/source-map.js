1f0198c16fd29bedd6f8f4f0597725044550d61d
keik <k4t0.kei@gmail.com>
2016-03-09 21:46:36 +0900

    Add test

---
+++ test/fixtures/source-map.js
@@ -0,0 +1,5 @@
+var debug = require('debug'),
+    fs    = require('fs')
+var d = debug('myapp')
+d(1)
+console.log(2)
--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
-var test = require('tape'),
-    read = require('fs').readFileSync,
-    path = require('path'),
+var test              = require('tape'),
+    read              = require('fs').readFileSync,
+    path              = require('path'),
+    convert           = require('convert-source-map'),
+    sourceMap         = require('source-map'),
+    SourceMapConsumer = sourceMap.SourceMapConsumer,
+
     fixture = function(f) {
       return read(path.join(__dirname, 'fixtures', f), 'utf8')
     }
@@ -58,6 +62,56 @@ test('udebug', function(t) {
             fixture('./import-and-call-expected.js'), 'calling debug are removed')
     t.equal(udebug(fixture('./import-and-call-on-scope.js')),
             fixture('./import-and-call-on-scope-expected.js'), 'calling debug are removed with scope sensitive')
+    t.end()
+  })
+
+  t.test('should bundle source map with `debug` option', function(t) {
+    var transformed = udebug(fixture('./source-map.js'), {
+      filepath: './source-map.js',
+      debug: true
+    })
+
+    /*
+     * original:
+     *
+     *   0123456789012345678901234
+     * 1 var debug = require('debug'),
+     * 2     fs    = require('fs')
+     * 3 var d = ('myapp')
+     * 4 d(1)
+     * 5 console.log(2)
+     */
+
+    /*
+     * transformed:
+     *
+     *   0123456789012345678901234
+     * 1 var fs = require('fs');
+     * 2 console.log(2);
+     */
+
+    var map = convert.fromSource(transformed).toObject(),
+        con = new SourceMapConsumer(map),
+        origPos
+
+    origPos = con.originalPositionFor({line: 1, column: 4})
+    t.equal(origPos.line, 2)
+    t.equal(origPos.column, 4)
+    origPos = con.originalPositionFor({line: 1, column: 9})
+    t.equal(origPos.line, 2)
+    t.equal(origPos.column, 12)
+    origPos = con.originalPositionFor({line: 1, column: 17})
+    t.equal(origPos.line, 2)
+    t.equal(origPos.column, 20)
+    origPos = con.originalPositionFor({line: 2, column: 0})
+    t.equal(origPos.line, 5)
+    t.equal(origPos.column, 0)
+    origPos = con.originalPositionFor({line: 2, column: 8})
+    t.equal(origPos.line, 5)
+    t.equal(origPos.column, 8)
+    origPos = con.originalPositionFor({line: 2, column: 12})
+    t.equal(origPos.line, 5)
+    t.equal(origPos.column, 12)
     t.end()
   })
 
